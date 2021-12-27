373590f817113f7a0532418dcd2cfc3d5005b2d5
Pratyaksh <tyagipratyaksh@gmail.com>
2021-12-27 00:51:34 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Pratyaksh Tyagi on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
